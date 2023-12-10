24cc6c62d4c231cfad63d26d4d918c5a0d266393
Ba1an <karolina_karo_balan@mail.ru>
2023-12-10 19:16:40 +0700

    2

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import os
 
 
+def acceptCommand():
+    k = 0
+    while k == 0:
+        request = input('Пожалуйста, введите номер выбранной команды: ')
+        if request not in ['1', '2', '3', '4', '5', '6', '7']:
+            print('Ошибка ввода!')
+        else:
+           k += 1
+
+
 def moveDown(currentDir):
     os.chdir(currentDir)
     nextdir = input('Введите имя подкаталога: ')
@@ -24,7 +34,18 @@ def countBytes(path):
     return count_bytes
 
 
-import os
+path_to_files = []
+def findFilles(target, path):
+    found_files = 0
+    for i in os.listdir(path):
+        if target.lower() in i.lower():
+            found_files += 1
+            path_to_files.append(path + '\\' + i)
+            print(i)
+            if found_files == 0:
+                print('Файлы не найдены')
+        if os.path.isdir(path + '\\' + i):
+            findFilles(target, path + '\\' + i)
 
 
 def main():
